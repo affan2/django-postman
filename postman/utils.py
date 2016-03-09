54dfc7863796713e03565cfdaf01e2463ef9e0c4
Ahmed Salman <ahmed.salman.noor@gmail.com>
2016-03-09 11:59:13 +0500

    pass the missing sender object to notification from postman create message

--- postman/utils.py
+++ postman/utils.py
@@ -93,7 +93,7 @@ def notify_user(object, action, site):
         return
     if notification:
         # the context key 'message' is already used in django-notification/models.py/send_now() (v0.2.0)
-        notification.send(users=[user], label=label, extra_context={'pm_message': object, 'pm_action': action})
+        notification.send(sender=object.sender, users=[user], label=label, extra_context={'pm_message': object, 'pm_action': action})
     else:
         if not DISABLE_USER_EMAILING and user.email and user.is_active:
             email('postman/email_user_subject.txt', 'postman/email_user.txt', [user.email], object, action, site)
